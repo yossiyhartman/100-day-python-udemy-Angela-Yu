2bdd1fc65ba9ef02cc74f8add64d752b3a5f5422
yossiyanouhartman <yossiyanouhartman@live.nl>
2023-08-24 11:15:30 +0200

    Completed day 1/100 coding challenge

---
+++ Day1_BandnameGenerator/bandNameGenerator.py
@@ -0,0 +1,8 @@
+print('Welcom to the Band Name Generator')
+
+city_name = input('What is the name of the city you grew up in?\n')
+pet_name = input("What is your pet's name?\n")
+
+print(f'Your band name could be {city_name} {pet_name}')
+
+#done
